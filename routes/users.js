88e067d445d5e2477b8643283af63adf4526e470
MatreRajesh <matrerajesh1234@gmail.com>
2024-01-06 13:03:49 +0530

    fixed engine

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/NetflixProject');
+mongoose.connect('mongodb+srv://netflix-clone-project:matrerajesh@cluster0.euurqwu.mongodb.net/');
 
 const userSchema =mongoose.Schema({
     username: {
